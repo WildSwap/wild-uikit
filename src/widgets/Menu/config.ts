11ffda20e80628da8f42bfd88888c32dac3d644a
WildSwap <85905532+WildSwap@users.noreply.github.com>
2021-06-15 00:33:26 +0200

    Add files via upload

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,102 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.wildswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.wildswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    href: "/ifo",
+  },
+
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+ 
+      {
+        label: "Github",
+        href: "https://github.com/wildswap/",
+      },
+
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/wildswap",
+      },
+
+      {
+        label: "Announcements",
+        href: "https://t.me/wildswap",
+      },
+
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/wildswap",
+  },
+ 
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
